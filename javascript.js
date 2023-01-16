b0004ea2975e305fe35136ebb9a5bcac5d7c3caf
cbridges <cbridges215@gmail.com>
2023-01-15 23:23:32 -0600

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello, world!")
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let choice = options[Math.floor(Math.random()*3)];
+    console.log(choice);
+    return choice;
+}
+
+getComputerChoice()
