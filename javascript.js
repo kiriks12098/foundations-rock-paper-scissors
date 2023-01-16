648e592c09541571b5225c923be96dd824dad3a9
cbridges <cbridges215@gmail.com>
2023-01-15 23:34:36 -0600

    Add function for determining winner/loser

--- javascript.js
+++ javascript.js
@@ -6,4 +6,34 @@ function getComputerChoice() {
     return choice;
 }
 
-getComputerChoice()
+function getWinner(computer, player) {
+    if (computer === player) {
+        console.log("It's a tie!");
+        return;
+    } else if (computer === "rock") {
+        if (player === "paper") {
+            console.log("You win!");
+        }
+        else {
+            console.log("You lose!");
+        }
+    } else if (computer === "paper") {
+        if (player === "scissors") {
+            console.log("You win!");
+        } else {
+            console.log("You lose!");
+        }
+    } else if (computer === "scissors") {
+        if (player === "rock") {
+            console.log("You win!");
+        } else {
+            console.log("You lose!")
+        }
+    }
+}
+
+
+let computerChoice = getComputerChoice();
+let playerChoice = prompt("Make a choice - Rock, Paper, or Scissors: ");
+
+getWinner(computerChoice, playerChoice.toLowerCase());
