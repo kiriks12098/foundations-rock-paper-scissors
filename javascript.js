f0818fc70ac5cc1ced885ed9e5c336e966aa6d87
cbridges <cbridges215@gmail.com>
2023-01-17 22:47:14 -0600

    Add rock, paper, scissors button and functionality

--- javascript.js
+++ javascript.js
@@ -23,32 +23,51 @@ function getWinner(computer, player) {
             win();
             return ("You win! Scissors beats paper!");
         } else {
-            return ("You lose! Paper beats rock!");
+            return ("You lose! Rock beats scissors!");
         }
     } else if (computer === "scissors") {
         if (player === "rock") {
             win();
             return ("You win! Rock beats scissors!");
         } else {
-            return ("You lose! Paper beats rock!")
+            return ("You lose! Scissors beats paper!")
         }
     }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
+    // for (let i = 0; i < 5; i++) {
         let computerChoice = getComputerChoice();
-        let playerChoice = prompt("Make a choice - Rock, Paper, or Scissors: ");
+        // let playerChoice = prompt("Make a choice - Rock, Paper, or Scissors: ");
 
         let winner = getWinner(computerChoice, playerChoice.toLowerCase());
 
         console.log(winner);
         console.log(`Your score is ${score}`);
     }
-}
+// }
 
 function win() {
     score++;
 }
 
-game()
+let playerChoice = "";
+
+const rock_button = document.querySelector("#rock");
+const paper_button = document.querySelector("#paper");
+const scissors_button = document.querySelector("#scissors");
+
+rock_button.addEventListener('click', () => {
+    playerChoice = "rock";
+    game();
+});
+
+paper_button.addEventListener('click', () => {
+    playerChoice = "paper";
+    game();
+});
+
+scissors_button.addEventListener('click', () => {
+    playerChoice = "scissors";
+    game();
+});
